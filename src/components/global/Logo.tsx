1c662f40df710c124bbae42423f050721598367c
jjribeiro1 <jvsr.99@hotmail.com>
2023-06-03 20:47:50 -0300

    fix: logo import

--- src/components/global/Logo.tsx
+++ src/components/global/Logo.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Image from 'next/image';
+import LogoSvg from '../../../public/logo.svg'
 
 export default function Logo() {
   return (
     <div className="relative md:h-12 md:w-20 lg:h-16 lg:w-24">
-      <Image src="./logo.svg" alt="logo da empresa" fill />
+      <Image src={LogoSvg} alt="logo da empresa" fill />
     </div>
   );
 }
