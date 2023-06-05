fb39e22055c7a28896ea9310aa40b9a4b7ca72aa
jjribeiro1 <jvsr.99@hotmail.com>
2023-06-05 11:21:19 -0300

    refactor: onClick logo redirect to home

--- src/components/global/Logo.tsx
+++ src/components/global/Logo.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import Link from 'next/link';
 import Image from 'next/image';
-import LogoSvg from '../../../public/logo.svg'
+import LogoSvg from '../../../public/logo.svg';
 
 export default function Logo() {
   return (
     <div className="relative md:h-12 md:w-20 lg:h-16 lg:w-24">
-      <Image src={LogoSvg} alt="logo da empresa" fill />
+      <Link href={'/'}>
+        <Image src={LogoSvg} alt="logo da empresa" fill />
+      </Link>
     </div>
   );
 }
