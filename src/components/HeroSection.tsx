73084126c2fe84795c2b9acec16ecbe15a5f6dd1
jjribeiro1 <jvsr.99@hotmail.com>
2023-06-07 21:23:25 -0300

    refactor: move hero slider to a component

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,7 @@
-'use client';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { HeroImages } from '@/types/hero-images';
 import Image from 'next/image';
+import HeroSlider from './HeroSlider';
 
 interface HeroSectionProps {
   images: HeroImages;
@@ -9,65 +9,11 @@ interface HeroSectionProps {
 const apiUrl = process.env.NEXT_PUBLIC_STRAPI_ASSETS_URL;
 
 export default function HeroSection({ images }: HeroSectionProps) {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [autoplay, setAutoplay] = useState(true);
   const { slides, banners } = images.data.attributes;
-
-  const handleDotClick = (newIndex: number) => {
-    setCurrentIndex(newIndex);
-  };
-  const stopAutoplay = () => {
-    setAutoplay(false);
-  };
-
-  useEffect(() => {
-    let autoplayTimer: NodeJS.Timer;
-
-    if (autoplay) {
-      autoplayTimer = setInterval(() => {
-        const isLastIndex = currentIndex === slides.data.length - 1;
-        setCurrentIndex(isLastIndex ? 0 : (prevState) => prevState + 1);
-      }, 5000);
-    }
-
-    return () => {
-      clearInterval(autoplayTimer);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentIndex]);  
-
   return (
     <>
       <section className="grid lg:grid-cols-2 lg:gap-6 w-full lg:h-[450px] xl:h-[500px] ">
-        <div className="relative w-full h-full overflow-hidden">
-          {slides.data.map((slide, i) => (
-            <Image
-              key={i}
-              src={`${apiUrl}${slide.attributes.formats.small.url}`}
-              alt="Imagem de um produto"
-              fill
-              sizes="(min-width: 375px) 100%"
-              quality={100}
-              className={`object-cover rounded-lg h-full w-full cursor-pointer duration-500 ${
-                currentIndex === i ? 'opacity-100 z-10' : 'opacity-0 z-0'
-              }`}
-            />
-          ))}
-          <div className="z-20 absolute left-1/2 bottom-5 -translate-x-1/2 flex items-center gap-3">
-            {slides.data.map((_, i) => (
-              <button
-                key={i}
-                className={`cursor-pointer h-[14px] w-[14px] border ${
-                  currentIndex === i ? 'bg-[#232f3e] border-[#232f3e]' : 'bg-white border-white'
-                }  rounded-full`}
-                onClick={() => {
-                  handleDotClick(i);
-                  stopAutoplay();
-                }}
-              ></button>
-            ))}
-          </div>
-        </div>
+        <HeroSlider slides={slides.data} />
 
         <div className="lg:grid lg:grid-cols-2 lg:gap-4">
           {banners.data.map((banner, i) => (
---
+++ src/components/HeroSlider.tsx
@@ -0,0 +1,71 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+import { StrapiImage } from '@/types/strapi-image';
+
+interface HeroSliderProps {
+  slides: StrapiImage[];
+}
+
+const apiUrl = process.env.NEXT_PUBLIC_STRAPI_ASSETS_URL;
+
+export default function HeroSlider({ slides }: HeroSliderProps) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [autoPlay, setAutoPlay] = useState(true);
+
+  const handleDotClick = (newIndex: number) => {
+    setCurrentIndex(newIndex);
+  };
+
+  const stopAutoPlay = () => {
+    setAutoPlay(false);
+  };
+
+  useEffect(() => {
+    let autoPlayTimer: NodeJS.Timer;
+
+    if (autoPlay) {
+      autoPlayTimer = setInterval(() => {
+        const isLastIndex = currentIndex === slides.length - 1;
+        setCurrentIndex(isLastIndex ? 0 : (prevState) => prevState + 1);
+      }, 5000);
+    }
+
+    return () => {
+      clearInterval(autoPlayTimer);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentIndex]);
+
+  return (
+    <div className="relative w-full h-full overflow-hidden">
+      {slides.map((slide, i) => (
+        <Image
+          key={i}
+          src={`${apiUrl}${slide.attributes.formats.small.url}`}
+          alt="Imagem de um produto"
+          fill
+          sizes="(min-width: 375px) 100%"
+          quality={100}
+          className={`object-cover rounded-lg h-full w-full cursor-pointer duration-500 ${
+            currentIndex === i ? 'opacity-100 z-10' : 'opacity-0 z-0'
+          }`}
+        />
+      ))}
+      <div className="z-20 absolute left-1/2 bottom-5 -translate-x-1/2 flex items-center gap-3">
+        {slides.map((_, i) => (
+          <button
+            key={i}
+            className={`cursor-pointer h-[14px] w-[14px] border ${
+              currentIndex === i ? 'bg-[#232f3e] border-[#232f3e]' : 'bg-white border-white'
+            }  rounded-full`}
+            onClick={() => {
+              handleDotClick(i);
+              stopAutoPlay();
+            }}
+          ></button>
+        ))}
+      </div>
+    </div>
+  );
+}
