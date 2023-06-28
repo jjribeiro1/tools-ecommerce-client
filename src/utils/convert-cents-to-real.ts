c6bdfd4ca0a29a61b89568cdaa8b19c587804a97
jjribeiro1 <jvsr.99@hotmail.com>
2023-06-28 18:50:10 -0300

    refactor convertCentsToReal function with intl api to format price

--- src/utils/convert-cents-to-real.ts
+++ src/utils/convert-cents-to-real.ts
@@ -1,1 +1,7 @@
-export const convertCentsToReal = (price: number) => (price / 100).toFixed(2);
+export const convertCentsToReal = (price: number) => {
+  const priceInReal = price / 100;
+  const formattedPrice = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(
+    priceInReal
+  );
+  return formattedPrice;
+};
