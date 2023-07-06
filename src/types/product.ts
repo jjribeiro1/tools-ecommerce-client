ca9c5e28447b730585e168fed9d256b2f8d590ef
jjribeiro1 <jvsr.99@hotmail.com>
2023-07-06 10:06:57 -0300

    refactor interface DailyDealsFetchResponse

--- src/types/product.ts
+++ src/types/product.ts
@@ -23,21 +23,5 @@ export interface Product {
 }
 
 export interface DailyDealsFetchResponse {
-  data: {
-    id: number;
-    attributes: {
-      name: string;
-      createdAt: Date;
-      updatedAt: Date;
-      publishedAt: Date;
-      originalPrice: number;
-      promotionalPrice: number;
-      discountPercentage: number;
-      discountIsActive: true;
-      dailyDeal: true;
-      images: {
-        data: StrapiImage[];
-      };
-    };
-  }[];
+  data: Product[];
 }
