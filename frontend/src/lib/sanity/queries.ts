e83b7d34cfdb08c03a03de998e8a2b7b717b0258
jjribeiro1 <jvsr.99@hotmail.com>
2023-11-26 16:00:45 -0300

    add getProductBySlug query and add priceOrder to getProductsFromCategorySlug query

--- frontend/src/lib/sanity/queries.ts
+++ frontend/src/lib/sanity/queries.ts
@@ -24,10 +24,22 @@ async function fetchDailyDealsProducts() {
 }
 const getDailyDeals = cache(fetchDailyDealsProducts);
 
-async function getProductsFromCategorySlug(slug: string) {
-  const query = `*[_type == 'product' && category->slug.current == '${slug}']`;
+async function getProductsFromCategorySlug(slug: string, priceOrder: string) {
+  const query = `*[_type == 'product' && category->slug.current == '${slug}'] | order(price ${priceOrder ?? ''})`;
   const data = await sanityClient.fetch<Product[]>(query);
   return data;
 }
 
-export { getCategoriesOverview, getPopularCategories, getDailyDeals, getProductsFromCategorySlug };
+async function getProductBySlug(slug: string) {
+  const query = `*[_type == 'product' && slug.current == '${slug}'][0]`;
+  const data = await sanityClient.fetch<Product>(query);
+  return data;
+}
+
+export {
+  getCategoriesOverview,
+  getPopularCategories,
+  getDailyDeals,
+  getProductsFromCategorySlug,
+  getProductBySlug,
+};
