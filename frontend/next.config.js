58b4a0c839c72439d657bb5fa64a85a007e2ab43
jjribeiro1 <jvsr.99@hotmail.com>
2023-11-25 21:42:02 -0300

    add sanity hostname to next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [process.env.STRAPI_DOMAIN],
+        domains: [process.env.STRAPI_DOMAIN, "cdn.sanity.io"],
     }
 }
 
