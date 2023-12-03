cf3040450d4eec86e737a5ae9c3ea8b631ecf41c
jjribeiro1 <jvsr.99@hotmail.com>
2023-12-02 22:53:36 -0300

    route handler to create stripe checkout session

---
+++ frontend/src/app/api/checkout/route.ts
@@ -0,0 +1,70 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { stripe } from '@/lib/stripe';
+import { urlFor } from '@/lib/sanity';
+import { fetchProductsInfoToValidate } from '@/lib/sanity/queries';
+import { CartStoreItem } from '@/app/store/cart';
+
+export async function POST(req: NextRequest) {
+  const originUrl = req.nextUrl.origin;
+  const body: CartStoreItem[] = await req.json();
+
+  try {
+    const productsFromSanity = await Promise.all(
+      body.map(({ product }) => fetchProductsInfoToValidate(product._id))
+    );
+    const invalidData = productsFromSanity.some(
+      (result, i) => result._id !== body[i].product._id || result.price !== body[i].product.price
+    );
+
+    if (invalidData) {
+      return NextResponse.json(
+        { message: 'Dados inválidos, não foi possível prosseguir para o checkout' },
+        { status: 400 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json({ message: 'Não foi possível prosseguir para o checkout', reason: error });
+  }
+
+  const formattedProducts = body.map(({ product, quantity }) => ({
+    _id: product._id,
+    name: product.name,
+    price: Math.round(Number(product.price * 100)),
+    quantity,
+    image: urlFor(product.images[0]).url(),
+  }));
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      mode: 'payment',
+      currency: 'brl',
+      locale: 'pt-BR',
+      payment_method_types: ['card'],
+      shipping_address_collection: {
+        allowed_countries: ['BR'],
+      },
+      after_expiration: {
+        recovery: {
+          enabled: true,
+        },
+      },
+      success_url: `${originUrl}/cart/?success=true`,
+      cancel_url: `${originUrl}/cart/?canceled=true`,
+      line_items: formattedProducts.map((product) => ({
+        price_data: {
+          currency: 'brl',
+          product_data: {
+            name: product.name,
+            images: [product.image],
+          },
+          unit_amount: product.price,
+        },
+        quantity: product.quantity,
+      })),
+    });
+
+    return NextResponse.json({ checkoutUrl: session.url });
+  } catch (error) {
+    return NextResponse.json({ message: 'Erro no servidor', reason: error }, { status: 500 });
+  }
+}
