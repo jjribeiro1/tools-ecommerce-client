554553497db34b4320eaa3269bf3839c69fc4498
jjribeiro1 <jvsr.99@hotmail.com>
2023-12-06 22:36:38 -0300

    reset stores after logout

--- frontend/src/Providers/index.tsx
+++ frontend/src/Providers/index.tsx
@@ -1,8 +1,19 @@
-'use client'
+'use client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useAuth } from '@clerk/nextjs';
+import { useCartStore } from '@/store/cart';
+import { useFavoriteItemsStore } from '@/store/favorites';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient();
+  const { isSignedIn } = useAuth();
+  const resetCartStore = useCartStore((state) => state.resetStore);
+  const resetFavoriteStore = useFavoriteItemsStore((state) => state.resetStore);
+  
+  if (!isSignedIn) {
+    resetCartStore();
+    resetFavoriteStore();
+  }
 
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
