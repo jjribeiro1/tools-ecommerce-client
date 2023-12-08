1edefe9bd3998c68e87caa77c0242f50c843e95d
jjribeiro1 <jvsr.99@hotmail.com>
2023-12-08 10:48:45 -0300

    remove unnecessary cache function

--- frontend/src/lib/sanity/queries.ts
+++ frontend/src/lib/sanity/queries.ts
@@ -1,29 +1,25 @@
-import { cache } from 'react';
 import { CategoryOverview, PopularCategories } from '@/types/category';
 import { sanityClient } from '.';
 import { Product, ProductWithCategory, ProductsInfoToValidate } from '@/types/product';
 import { HeroSection } from '@/types/hero-section';
 
-async function fetchCategoriesOverview() {
+async function getCategoriesOverview() {
   const query = `*[_type == 'category']{_id, name, slug}`;
   const data = await sanityClient.fetch<CategoryOverview[]>(query);
   return data;
 }
-const getCategoriesOverview = cache(fetchCategoriesOverview);
 
-async function fetchPopularCategories() {
+async function getPopularCategories() {
   const query = `*[_type == 'category' && isPopular == true]{_id, name, slug, isPopular, image}`;
   const data = await sanityClient.fetch<PopularCategories[]>(query);
   return data;
 }
-const getPopularCategories = cache(fetchPopularCategories);
 
-async function fetchDailyDealsProducts() {
+async function getDailyDeals() {
   const query = `*[_type == 'product' && dailyDeal == true]`;
   const data = await sanityClient.fetch<Product[]>(query);
   return data;
 }
-const getDailyDeals = cache(fetchDailyDealsProducts);
 
 async function getProductsFromCategorySlug(slug: string, priceOrder: string) {
   const query = `*[_type == 'product' && category->slug.current == '${slug}'] | order(price ${
@@ -33,19 +29,18 @@ async function getProductsFromCategorySlug(slug: string, priceOrder: string) {
   return data;
 }
 
-async function fetchProductBySlug(slug: string) {
+async function getProductBySlug(slug: string) {
   const query = `*[_type == 'product' && slug.current == '${slug}']{_id, name, slug, price, promotionalPrice, discountPercentage, discountIsActive, dailyDeal, description, images, category->{name, slug}}[0]`;
   const data = await sanityClient.fetch<ProductWithCategory>(query);
   return data;
 }
-const getProductBySlug = cache(fetchProductBySlug);
 
-async function fetchHeroSectionData() {
+async function getHeroSectionData() {
   const query = `*[_type == 'heroSection'][0]`;
   const data = await sanityClient.fetch<HeroSection>(query);
   return data;
 }
-const getHeroSectionData = cache(fetchHeroSectionData);
+
 
 async function fetchProductsInfoToValidate(id: string) {
   const query = `*[_type == 'product' && _id == '${id}']{_id, price}[0]`;
